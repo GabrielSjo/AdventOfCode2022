0f71151915f4efe43c3e4d73eab2c298bb8fc509
GabrielSjo <gabsjo94@gmail.com>
2022-12-07 00:38:30 +0100

    added dec 4,5

---
+++ Dec4/Dec4-2.py
@@ -0,0 +1,13 @@
+count = 0
+with open('Dec4/input.txt') as input:
+    for line in input:
+        line = line.strip().split(',')
+        section = []
+        for elf in line:
+            section.append(elf.split('-'))
+        section = [list(map(int,i)) for i in section]
+        if (section[0][0] >= section[1][0] and section[0][0] <= section[1][1]) or (section[0][1] <= section[1][1] and section[0][1] >= section[1][0]):
+            count += 1
+        elif (section[0][0] <= section[1][0] and section[1][0] <= section[0][1]) or (section[0][1] >= section[1][1] and section[1][1] >= section[0][0]):
+            count += 1
+    print(count)
